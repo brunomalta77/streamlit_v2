0f72c2720e2ae563c6a6b37df519ebf70b80c2a1
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2023-07-26 12:44:35 +0100

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -15,9 +15,13 @@ import glob
 import os
 
 #Getting the API_Keys
-load_dotenv()
-api_key = os.getenv('API_Keys')
-openai.api_key= api_key
+#load_dotenv()
+#api_key = os.getenv('API_Keys')
+#openai.api_key= api_key
+
+api_key = st.secrets["API_KEY"]
+
+
 
 
 #page config
@@ -183,6 +187,7 @@ def main():
             #market = st.text_input("Enter your market here")
             #path_name = f"C:\\Users\\BrunoMalta\\Brand Delta\\Food Pilot - General\\data\\modelled_data\\{market}\\Workflow_output\\latest_output"
             #file = glob.glob(path_name + "/*.parquet")
+            st.write(api_key)
             df_file= st.file_uploader("Upload a Parquet file")
             if df_file is not None:
                 # read our file
