623e3e8df1bb07ff6518fc19d66cdf3122a928ad
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2023-07-26 15:37:20 +0100

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -166,6 +166,7 @@ def to_excel(df):
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
     df.to_excel(writer, index=False, sheet_name='Sheet1')
+    st.write(df)
     workbook = writer.book
     worksheet = writer.sheets['Sheet1']
     format1 = workbook.add_format({'num_format': '0.00'}) 
