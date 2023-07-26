6545764db88d82012d151742f4da0d41fedaa964
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2023-07-26 15:42:51 +0100

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -166,14 +166,14 @@ def to_excel(df):
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
     df.to_excel(writer, index=False, sheet_name='Sheet1')
-    #st.write(df)
-    #workbook = writer.book
-    #worksheet = writer.sheets['Sheet1']
-    #format1 = workbook.add_format({'num_format': '0.00'}) 
-    #worksheet.set_column('A:A', None, format1)  
-    ##writer.save()
-    #processed_data = output.getvalue()
-    return df
+    st.write(df)
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+    format1 = workbook.add_format({'num_format': '0.00'}) 
+    worksheet.set_column('A:A', None, format1)  
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
     
 
 
