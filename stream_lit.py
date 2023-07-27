de6d8b2e69368545e602f2d9ab1f893f62b63a3d
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2023-07-27 11:03:46 +0100

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -321,13 +321,13 @@ def main():
 
 
                 # saving
-                        if st.session_state.button is not None:
-                            if st.checkbox("Save"):
-                                df_xlsx = to_excel(st.session_state.df_final)
-                                st.download_button(label='📥 Download Current Topics',
-                                data=df_xlsx,
-                                file_name= f"{st.session_state.brand_name}{st.session_state.name_file}.xlsx")
-                                st.write("save successful")
+                    if st.session_state.button is not None:
+                        if st.checkbox("Save"):
+                            df_xlsx = to_excel(st.session_state.df_final)
+                            st.download_button(label='📥 Download Current Topics',
+                            data=df_xlsx,
+                            file_name= f"{st.session_state.brand_name}{st.session_state.name_file}.xlsx")
+                            st.write("save successful")
 
 
 
