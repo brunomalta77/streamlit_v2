14a9ee452640637b4609bfd5294ac50df7adf1ee
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2023-07-27 11:08:05 +0100

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -336,7 +336,7 @@ def main():
                             st.write("\n")
                             st.write(top_topics)
                             df_xlsx = to_excel(final_df)
-                            st.download_button(label='📥 Download Current Topics', data=df_xlsx, file_name= f"{st.session_state.brand_name}{name_file}.xlsx")
+                            st.download_button(label='📥 Download Current Topics', data=df_xlsx, file_name=  f"{st.session_state.brand_name}{st.session_state.name_file}.xlsx")
 
 
 
