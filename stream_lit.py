80768a21821253d9226f2382bd6f7fa6886173a3
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2023-07-27 12:57:44 +0100

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -68,7 +68,8 @@ def my_values_all(df):
 def my_values_without_author(df,ws=None,we=None):
     channel_options= [x for x in df["message_type"].unique()]
     if ws is None and we is None:
-        #channel = st.multiselect("Select the channel categories:", channel_options)
+        channel = st.multiselect("Select the channel categories:", channel_options)
+        st.write(channel_options)
         return channel_options
     else:
     #time period
@@ -95,7 +96,7 @@ def filtering_all(df,author,channel):
 
 def filtering_without_author(df,channel,ws=None,we=None):
     if ws is None and we is None:
-        df = df[(df["message_type"].isin(channel))]
+        df = df[df["message_type"].isin(channel)]
         alldata = ' '.join(df["cleaned_message"])
         lengpt = len(alldata) / 4000   #(because chatgot maximum token size is 4076)
         posts_to_combine = round(len(df) / lengpt)
