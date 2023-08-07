ca7fed4d59cb0327c3ce5d2f20a5ed6349c787f4
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2023-08-07 10:28:06 +0100

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -272,7 +272,7 @@ def main():
                 st.session_state.brand_name = file_name
                 # read our file
                 #st.session_state.df['Week Commencing'] = st.session_state.df['created_time'].apply(lambda x: (x - timedelta(days=x.weekday())).replace(hour=0, minute=0, second=0, microsecond=0))
-                st.write(st.session_state_df.columns)
+                st.write(st.session_state.df.columns)
                 st.info(f"Data size : {st.session_state.df.shape[0]}")
                 if st.session_state.df is not None:
                     if st.checkbox("Filtered data"):
