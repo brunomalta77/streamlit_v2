e12d36642a61d3ce9a25b008f1d62673e80a0124
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2023-08-07 10:54:46 +0100

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -34,7 +34,8 @@ st.title("Brand Delta Topic Modelling")
 @st.cache(allow_output_mutation=True,suppress_st_warning=True) 
 def read_excel(df_file):
     with st.spinner("Reading your file...."):
-        df = pd.read_excel(df_file)
+        with contextlib.suppress(pd.errors.ParserWarning):
+            df = pd.read_excel(df_file)
         uploaded_file_info= str(df_file)
         file_name = uploaded_file_info.split(", name='")[1].split(".")[0]
         df['Week Commencing'] = df['created_time'].apply(lambda x: (x - timedelta(days=x.weekday())).replace(hour=0, minute=0, second=0, microsecond=0))
