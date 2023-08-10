fa321ddba95f019af9a7993493ee33883a14f09b
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2023-08-10 15:41:37 +0100

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -447,7 +447,10 @@ def main():
                             st.download_button(label='📥 Download Current Topics',
                             data=df_xlsx,
                             file_name= f"{st.session_state.brand_name}{st.session_state.name_file}.xlsx")
-                            st.write("save successful")
+                            if st.download_button:
+                                st.write("save successful")
+                            else:
+                                st.warning("click on the download button to download") 
                         if st.checkbox("Change topics"):
                             number_options = list(range(1,11))
                             selected_number = st.selectbox("Num of topics",number_options)
