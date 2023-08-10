45bdb9b86ab569b07b65b974f358cca61a260c0c
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2023-08-10 17:39:03 +0100

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -463,7 +463,8 @@ def main():
                             st.write("\n")
                             st.write(top_topics)
                             df_xlsx = to_excel(final_df)
-                            if st.download_button(label='📥 Download Current Topics', data=df_xlsx, file_name=  f"{st.session_state.brand_name}{st.session_state.name_file}_{selected_number}_topics.xlsx"):
+                            if st.download_button(label='📥 Download Current Topics', 
+                                                  data=df_xlsx, file_name=  f"{st.session_state.brand_name}{st.session_state.name_file}_{selected_number}_topics.xlsx"):
                                 press = True
                             if press == True:
                                 st.write("save successful")
