fe06d998c71d152adf99251a3fdf07040c681116
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2023-09-12 22:22:28 +0100

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -163,8 +163,8 @@ def filtering_without_author(df,channel,brand,ws=None,we=None):
 
 
 # generating the Chat GPT respose
-@st.cache(allow_output_mutation=True,suppress_st_warning=True,n) 
-def generate_chatgpt_response_v2(prompt, model = "gpt-3.5-turbo-16k"):
+@st.cache(allow_output_mutation=True,suppress_st_warning=True) 
+def generate_chatgpt_response_v2(prompt, model = "gpt-3.5-turbo-16k",n):
     time.sleep(n)
     responses = []
     restart_sequence = "\n"
