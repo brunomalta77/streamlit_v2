3149e445751dc04386d9ef778b74ae3bf4427b2b
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2023-09-12 22:39:22 +0100

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -203,7 +203,7 @@ def get_topics(df,n):
         end_time= time.time()
         elapsed_time = end_time - start_time
         # Print progress update
-        st.write(f"Processing request {i + 1} of {total_requests} ({progress:.2f}% complete), time:{elapsed_time}")
+        st.write(f"Processing request {i + 1} of {total_requests} ({progress:.2f}% complete), time:{round(elapsed_time,2)}")
         time.sleep(2)
     
     # Merging the topics with the actual dataframe
